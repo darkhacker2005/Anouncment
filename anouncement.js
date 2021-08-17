707f465be20a4b19a0913bc78454bba736b6c2b7
darkhacker2005 <85599878+darkhacker2005@users.noreply.github.com>
2021-08-17 07:44:59 +0530

    Create anouncement.js

---
+++ anouncement.js
@@ -0,0 +1,131 @@
+{
+
+    "WhatsAsena":true,
+
+    "__ASENAJSON__":true,
+
+    "variables":{
+
+        "user":"පරිශීලක නාමය",
+
+        "wa_version":"පරිශීලක Whatsapp version",
+
+        "version":"DARK KNIGHT Version",
+
+        "os_version":"පරිශීලක පද්ධති version",
+
+        "device_model":"පරිශීලක Device Model",
+
+        "device_brand":"User Device Brand (Apple,HTC,Redmi,Samsung, Nokia..)"
+
+    },
+
+    "announcements":{
+
+        "infoen":"〔 *New  news‼️* 〕\n\nHello📢 *DARK KNIGHT BOT users.* \nDo you know our easy way to solve your problems `` `DARK KNIGHT TEAM``` started * DARK KNIGHT HELP CENTER *\n\nIt only works if you have received the latest update \n\n*.dhelp*  You can access that service with this command\n```- DARK KNIGHT Team -```",
+
+        "infosi":"〔 *අලුත් පණිවිඩ‼️* 〕\n\nHello📢 *DARK KNIGHT users.* \nඔබ දන්නවාද ඔබගේ ප්‍රශ්න වලට ලේසියෙන්ම විසදුම් ගැනීමට අපේ ```DARK KNIGHT TEAM``` එක විසින් *DARK KNIGHT HELP CENTER* ආරම්භ කරා\n\nඔබ නවතම යාවත් කාලීන කිරීම ලබා ඇත්නම් එක වැඩ කරයි \n\n*.dhelp*  මෙම විදානයෙන් ඔබට එම සේවාව ලබා ගත හැක\n```- DARK KNIGHT Team -```"
+
+    },
+
+    "status":"200",
+
+    "announcement_owner":"HIRUWA",
+
+    "announcement_time":"GMT+3",
+
+    "stack":"/root/rn432/bot.js",
+
+    "languages":[
+
+        "SI",
+
+        "EN"
+
+    ],
+
+    "languages_count":"2",
+
+    "whatsasena_info":{
+
+        "name":"DARK KNIGHT",
+
+        "version":"3.1",
+
+        "description":"DARK KNIGHT is a sinhala chat bot for WhatsApp.",
+
+        "legacy_repo":"https://github.com/darkhacker2005/slRavana",
+
+        "new_repo":"https://github.com/darkhacker2005/slRavana",
+
+        "logo_url":{
+
+            "raw":"https://telegra.ph/file/b0f60558bf2d8d354455f.jpg",
+
+            "hd+":"https://telegra.ph/file/b0f60558bf2d8d354455f.jpg",
+
+            "hd":"https://telegra.ph/file/b0f60558bf2d8d354455f.jpg",
+
+            "sd":"https://telegra.ph/file/b0f60558bf2d8d354455f.jpg",
+
+            "thumb":"https://telegra.ph/file/b0f60558bf2d8d354455f.jpg"
+
+        },
+
+        "developers":[
+
+            "DARK KNIGHT"
+
+        ],
+
+        "developers_descriptions":[
+
+            "Author, Base, Bug Fixes, Developer, Modules"
+
+        ],
+
+        "lisance":"GNU General Public Licence v3.0",
+
+        "whatsasena_desc":"DARK KNIGHT is a sinhala chat bot for WhatsApp.",
+
+        "issues":"https://github.com/darkhacker2005/slRavana/issues",
+
+        "pull_requests":"https://github.com/darkhacker2005/slRavana/pulls",
+
+        "wiki":"https://github.com/darkhacker2005/slRavana"
+
+    },
+
+    "details":[
+
+        {
+
+            "lang":"SI",
+
+            "desc":"DARK KNIGHT භාවිතා කරන්නන් සඳහා නිල නිවේදන.",
+
+            "repository":"https://github.com/darkhacker2005/slRavana",
+
+            "owner":"HIRUWA",
+
+            "owner_github":"https://github.com/darkhacker2005"
+
+        },
+
+        {
+
+            "lang":"EN",
+
+            "desc":"Official announcements for DARK KNIGHT Users.",
+
+            "repository":"https://github.com/darkhacker2005/slRavana",
+
+            "owner":"HIRUWA",
+
+            "owner_github":"https://github.com/darkhacker2005"
+
+        }
+
+    ]
+
+}
